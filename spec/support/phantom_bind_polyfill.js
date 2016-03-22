48d3db14ebf11f6f7ecf0c87f31d90e68ac36486
scrubmx <scrub.mx@gmail.com>
2016-03-22 11:24:29 -0600

    Update phantom_bind_polyfill.js
    Updated the phantom `bind` polyfill function.
    
    https://github.com/tom-james-watson/phantomjs-polyfill/blob/master/bind-polyfill.js

--- spec/support/phantom_bind_polyfill.js
+++ spec/support/phantom_bind_polyfill.js
@@ -1,40 +1,18 @@
-/////////////////////////////////////
-// Taken from ariya/phantomjs#10522
-/////////////////////////////////////
-
-Function.prototype.bind = function bind(that) { // .length is 1
-      var target = this;
-      if (typeof target != "function") {
-          throw new TypeError("Function.prototype.bind called on incompatible " + target);
-      }
-      var args = Array.prototype.slice.call(arguments, 1); // for normal call
-      var bound = function () {
-
-          if (this instanceof bound) {
-
-              var result = target.apply(
-                  this,
-                  args.concat(Array.prototype.slice.call(arguments))
-              );
-              if (Object(result) === result) {
-                  return result;
-              }
-              return this;
-
-          } else {
-              return target.apply(
-                  that,
-                  args.concat(Array.prototype.slice.call(arguments))
-              );
-
-          }
-
-      };
-      function Empty() {};
-      if(target.prototype) {
-          Empty.prototype = target.prototype;
-          bound.prototype = new Empty();
-          Empty.prototype = null;
-      }
-      return bound;
-  };
+if (typeof Function.prototype.bind != 'function') {
+    Function.prototype.bind = function bind(obj) {
+        var args = Array.prototype.slice.call(arguments, 1),
+            self = this,
+            nop = function() {
+            },
+            bound = function() {
+                return self.apply(
+                    this instanceof nop ? this : (obj || {}), args.concat(
+                        Array.prototype.slice.call(arguments)
+                    )
+                );
+            };
+        nop.prototype = this.prototype || {};
+        bound.prototype = new nop();
+        return bound;
+    };
+}
